3b9f7eca02bde874e6db423998922ce3b15e16d2
JCGoran <goran.cizmek@gmail.com>
2020-03-23 19:27:32 +0100

    Added redirection when starting a meeting

--- background.js
+++ background.js
@@ -6,16 +6,19 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
   }
 
   const url = new URL(details.url);
-  const match = /^\/j\/(\d+)\/?$/.exec(url.pathname);
+  const match = /^\/[j|s]\/(\d+)\/?$/.exec(url.pathname);
   if (match === undefined || match[1] === undefined) {
     return;
   }
 
+  const ending = match[0][1];
+  const mapping = {'j' : '/join', 's' : '/start'};
+
   // Save a round trip if the user requested a non-https url
   // At time of writing, Zoom has non-preloaded HSTS deployed
   url.protocol = 'https:';
-  url.pathname = '/wc/' + encodeURIComponent(match[1]) + '/join';
+  url.pathname = '/wc/' + encodeURIComponent(match[1]) + mapping[ending];
   return {
     redirectUrl: url.href
   }
-}, { urls: ['*://*.zoom.us/j/*'] }, ['blocking']);
+}, { urls: ['*://*.zoom.us/j/*', '*://*.zoom.us/s/*'] }, ['blocking']);
