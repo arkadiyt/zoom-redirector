f04146bf6d9dbb819442283004681cd97dd7eb97
Arkadiy Tetelman <arkadiyt@gmail.com>
2020-03-27 08:33:50 -0700

    Minor touchup

--- background.js
+++ background.js
@@ -6,13 +6,13 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
   }
 
   const url = new URL(details.url);
-  const match = /^\/[j|s]\/(\d+)\/?$/.exec(url.pathname);
+  const match = /^\/[js]\/(\d+)\/?$/.exec(url.pathname);
   if (match === undefined || match[1] === undefined) {
     return;
   }
 
   const ending = match[0][1];
-  const mapping = {'j' : '/join', 's' : '/start'};
+  const mapping = {'j': '/join', 's': '/start'};
 
   // Save a round trip if the user requested a non-https url
   // At time of writing, Zoom has non-preloaded HSTS deployed
@@ -20,5 +20,5 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
   url.pathname = '/wc/' + encodeURIComponent(match[1]) + mapping[ending];
   return {
     redirectUrl: url.href
-  }
+  };
 }, { urls: ['*://*.zoom.us/j/*', '*://*.zoom.us/s/*'] }, ['blocking']);
