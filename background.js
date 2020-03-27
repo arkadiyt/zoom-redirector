4dd3935e19b45402537d14d324bd3b1fe714f0ba
Arkadiy Tetelman <arkadiyt@gmail.com>
2020-03-27 11:49:10 -0700

    Add another redirect for Zoom's handling of GDPR

--- background.js
+++ background.js
@@ -6,19 +6,26 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
   }
 
   const url = new URL(details.url);
-  const match = /^\/[js]\/(\d+)\/?$/.exec(url.pathname);
-  if (match === undefined || match[1] === undefined) {
-    return;
+  url.protocol = 'https:';
+
+  // Rewrite zoom.us/j/<id> -> zoom.us/wc/<id>/join
+  // Rewrite zoom.us/s/<id> -> zoom.us/wc/<id>/start
+  let match = /^\/[js]\/(\d+)\/?$/.exec(url.pathname);
+  if (match !== null && match[1] !== undefined) {
+    const ending = match[0][1];
+    const mapping = {'j': '/join', 's': '/start'};
+    url.pathname = '/wc/' + encodeURIComponent(match[1]) + mapping[ending];
+    return {
+      redirectUrl: url.href
+    };
   }
 
-  const ending = match[0][1];
-  const mapping = {'j': '/join', 's': '/start'};
-
-  // Save a round trip if the user requested a non-https url
-  // At time of writing, Zoom has non-preloaded HSTS deployed
-  url.protocol = 'https:';
-  url.pathname = '/wc/' + encodeURIComponent(match[1]) + mapping[ending];
-  return {
-    redirectUrl: url.href
-  };
-}, { urls: ['*://*.zoom.us/j/*', '*://*.zoom.us/s/*'] }, ['blocking']);
+  // Rewrite zoom.us/wc/join/<id> -> zoom.us/wc/<id>/join
+  match = /^\/wc\/join\/(\d+)\/?$/.exec(url.pathname);
+  if (match !== null && match[1] !== undefined) {
+    url.pathname = '/wc/' + encodeURIComponent(match[1]) + '/join';
+    return {
+      redirectUrl: url.href
+    };
+  }
+}, { urls: ['*://*.zoom.us/j/*', '*://*.zoom.us/s/*', '*://*.zoom.us/wc/join/*'] }, ['blocking']);
