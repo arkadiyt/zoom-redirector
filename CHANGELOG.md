f8854d3a2a6eafca30ea1757cdef53f4a42e85d8
Arkadiy Tetelman <arkadiyt@users.noreply.github.com>
2023-02-15 22:53:38 -0800

    Chrome updates (#20)
    * Rename extension
    
    * Use v3 manifest
    
    * Version 1.0.3
    
    * Added toggle functionality
    
    * Handle top level domains

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+### 1.0.5 (05/28/2022)
+* Support redirects on top-level zoom domains (without subdomains)
+
+### 1.0.4 (04/15/2022)
+* Added the ability to toggle the extension by clicking on its icon
+
+### 1.0.3 (03/26/2022)
+* Republished the extension under the name Xoom Redirector, due to a [trademark violation notice from Zoom](https://twitter.com/arkadiyt/status/1500141960065028098)
+* Updated the extension to use Chrome's v3 manifest format
+
 ### 1.0.2 (03/27/2020)
 * Add support for zoomgov.com ([pmosbach](https://github.com/arkadiyt/zoom-redirector/pull/9))
 
--- background.js
+++ background.js
@@ -1,24 +1,16 @@
-'use strict';
+const badgeColor = [255, 0, 0, 255];
+const ruleIds = ['zoom-redirect'];
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  if (details.type !== 'main_frame' || details.method !== 'GET') {
-    return;
-  }
-
-  const url = new URL(details.url);
-  const match = /^\/[js]\/(\d+)\/?$/.exec(url.pathname);
-  if (match === undefined || match[1] === undefined) {
-    return;
-  }
-
-  const ending = match[0][1];
-  const mapping = {'j': '/join', 's': '/start'};
-
-  // Save a round trip if the user requested a non-https url
-  // At time of writing, Zoom has non-preloaded HSTS deployed
-  url.protocol = 'https:';
-  url.pathname = '/wc/' + encodeURIComponent(match[1]) + mapping[ending];
-  return {
-    redirectUrl: url.href
+chrome.action.onClicked.addListener(async () => {
+  const disabled = !(await chrome.storage.local.get(['disabled'])).disabled;
+  const badgeText = disabled ? 'X' : '';
+  const ruleSetOptions = disabled ? {
+    disableRulesetIds: ruleIds
+  } : {
+    enableRulesetIds: ruleIds
   };
-}, { urls: ['*://*.zoom.us/j/*', '*://*.zoom.us/s/*', '*://*.zoomgov.com/j/*', '*://*.zoomgov.com/s/*'] }, ['blocking']);
+  chrome.action.setBadgeText({text: badgeText});
+  chrome.action.setBadgeBackgroundColor({color: badgeColor});
+  chrome.declarativeNetRequest.updateEnabledRulesets(ruleSetOptions);
+  chrome.storage.local.set({disabled: disabled});
+});
